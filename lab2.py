08cc7e277c86e5217d132ff4c2126418f9fb4ad0
samee <sameeahsan.23@ichat.sp.edu.sg>
2024-04-24 16:16:50 +0800

    lab 2 without ex6

---
+++ lab2.py
@@ -0,0 +1,45 @@
+def display_main_menu():
+    print("display_main_menu")	
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+
+def get_user_input():
+    print("get_user_input")
+    getVal = input()
+    numbers = getVal.split(",")
+    # convertedNums = []
+    # for char in numbers:                      this is another way the strings / numbers in the list can be converted
+    #     convertedNums.append(int(char))       into a list of floats  
+
+    for i in range(len(numbers)):
+        numbers.append(float(numbers[0]))
+        numbers.pop(0)
+
+    return numbers
+
+
+def calc_average_temperature(valueList):
+    print("calc_average_temperature")  
+    total = sum(valueList)
+    average = total / len(valueList)
+    print(f"The average temperature value is {average}")
+    return average
+
+
+def calc_min_max_temperature(valueList):
+    print("calc_min_max_temperature")
+    minVal = min(valueList)
+    maxVal = max(valueList)
+    return [minVal, maxVal]   
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    calc_average_temperature(num_list)
+    print(f"The minimum and maximum temperature values are {calc_min_max_temperature(num_list)}")
+
+
+if __name__ == "__main__":
+    main()
