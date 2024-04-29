28a51a3c6821924e15a8e3605defdf2792a76c79
samee <sameeahsan.23@ichat.sp.edu.sg>
2024-04-29 16:16:57 +0800

    changes to bmi file

--- bmi.py
+++ bmi.py
@@ -3,10 +3,18 @@ def calculate_bmi(height, weight):
  print("Weight = " + str(weight))
  bmi = weight / (height * height)
  print("BMI = " + str(bmi))
+
  if bmi < 18.5:
   print("Under Weight")
- elif bmi > 18.5 and bmi < 25.0:
+  return -1
+ 
+ elif bmi >= 18.5 and bmi < 25.0:
   print("Normal Weight")
- else:
+  return 0
+ 
+ elif bmi >= 25.0 :
   print("Over Weight") 
+  return 1
+
+
 calculate_bmi(weight=57, height=1.73)
